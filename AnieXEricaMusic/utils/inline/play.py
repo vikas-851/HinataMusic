74fad7feb431d5f6233c0b61cbdf366c39445c5d
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2024-03-12 05:40:22 +0530

    ++

--- AnieXEricaMusic/utils/inline/play.py
+++ AnieXEricaMusic/utils/inline/play.py
@@ -59,52 +59,34 @@ def stream_markup_timer(_, chat_id, played, dur):
             )
         ],
         [
-            InlineKeyboardButton(
-                text=_["S_B_1 "], url=f"https://t.me/{app.username}?startgroup=s&admin=delete_messages+manage_video_chats+pin_messages+invite_users+"
-            ),
-        ],
-        [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-        [
-            InlineKeyboardButton(
-                text=_["S_B_2"], url=f"{SUPPORT_CHAT}"
-            ),
-            InlineKeyboardButton(
-                text=_["S_B_5"], url=f"t.me/{OWNER_USERNAME}"
-            ),
-        ],
         
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+      [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
 
 
 def stream_markup(_, chat_id):
     buttons = [
-         [
+
+        [
             InlineKeyboardButton(
-                text=_["S_B_1 "], url=f"https://t.me/{app.username}?startgroup=s&admin=delete_messages+manage_video_chats+pin_messages+invite_users+"
-            ),
+                text=f"{played} {bar} {dur}",
+                callback_data="GetTimer",
+            )
         ],
+
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-         [
-            InlineKeyboardButton(
-                text=_["S_B_2"], url=f"{SUPPORT_CHAT}"
-            ),
-            InlineKeyboardButton(
-                text=_["S_B_5"], url=f"t.me/{OWNER_USERNAME}"
-            ),
-        ],
-    
+     
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
