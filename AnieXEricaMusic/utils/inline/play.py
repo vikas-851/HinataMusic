ecac5d854d2d1a476a7d616e264fb9c7e67c08ad
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2024-03-31 18:08:16 +0530

    Update play.py

--- AnieXEricaMusic/utils/inline/play.py
+++ AnieXEricaMusic/utils/inline/play.py
@@ -61,7 +61,6 @@ def stream_markup_timer(_, chat_id, played, dur):
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="✮", callback_data=f"add_playlist {videoid}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
