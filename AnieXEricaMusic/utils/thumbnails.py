b286c85df6d65d3606c63665e2dcf32620edaf46
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2024-04-04 21:53:33 +0530

    Update thumbnails.py

--- AnieXEricaMusic/utils/thumbnails.py
+++ AnieXEricaMusic/utils/thumbnails.py
@@ -71,10 +71,10 @@ async def get_thumb(videoid):
         logo = ImageOps.expand(logo, border=15, fill="white")
         background.paste(logo, (50, 100))
         draw = ImageDraw.Draw(background)
-        font = ImageFont.truetype("AnieXEricaMusic/assets/font2.ttf", 40)
-        font2 = ImageFont.truetype("AnieXEricaMusic/assets/font2.ttf", 70)
-        arial = ImageFont.truetype("AnieXEricaMusic/assets/font2.ttf", 30)
-        name_font = ImageFont.truetype("AnieXEricaMusic/assets/font.ttf", 30)
+        font = ImageFont.truetype("AnieXEricaMusic/test/font2.ttf", 40)
+        font2 = ImageFont.truetype("AnieXEricaMusic/test/font2.ttf", 70)
+        arial = ImageFont.truetype("AnieXEricaMusic/test/font2.ttf", 30)
+        name_font = ImageFont.truetype("AnieXEricaMusic/test/font.ttf", 30)
         para = textwrap.wrap(title, width=32)
         j = 0
         draw.text(
