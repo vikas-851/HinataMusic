6bee47741cf78ab60090f07eee35591efb18952b
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2024-04-05 21:43:53 +0530

    Update start.py

--- AnieXEricaMusic/utils/inline/start.py
+++ AnieXEricaMusic/utils/inline/start.py
@@ -26,8 +26,7 @@ def private_panel(_):
             )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
-            InlineKeyboardButton(text=_["S_B_10"], callback_data="AMBOT_settings_back_helper"),
+            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")
         ],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
