1d96140c742e2cdd0ba7675da60eb9d93b495724
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2024-04-05 21:48:29 +0530

    Update help.py

--- AnieXEricaMusic/utils/inline/help.py
+++ AnieXEricaMusic/utils/inline/help.py
@@ -151,3 +151,59 @@ def private_help_panel(_):
         ],
     ]
     return buttons
+
+
+
+
+def AMBOThelp_pannel(_, START: Union[bool, int] = None):
+    first = [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data=f"close")]
+    second = [
+        InlineKeyboardButton(
+            text=_["BACK_BUTTON"],
+            callback_data=f"AMBOTsettingsback_helper",
+        ),
+    ]
+    mark = second if START else first
+    upl = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text=_["H_B_1"],
+                    callback_data="help_callback hb1",
+                ),
+                InlineKeyboardButton(
+                    text=_["H_B_2"],
+                    callback_data="help_callback hb2",
+                ),
+            ],
+            mark,
+        ]
+    )
+    return upl
+
+
+def AMBOThelp_back_markup(_):
+    upl = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text=_["BACK_BUTTON"],
+                    callback_data=f"AMBOTsettings_back_helper",
+                ),
+            ]
+        ]
+    )
+    return upl
+
+
+def AMBOTprivate_help_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["S_B_4"],
+                url=f"https://t.me/{app.username}?start=AMBOThelp",
+            ),
+        ],
+    ]
+    return buttons
+
