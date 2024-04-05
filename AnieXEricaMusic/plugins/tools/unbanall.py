e7a1c904ec4cce3241847f0b1f8e7322d3c88447
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2024-04-05 13:49:00 +0530

    Update unbanall.py

--- AnieXEricaMusic/plugins/tools/unbanall.py
+++ AnieXEricaMusic/plugins/tools/unbanall.py
@@ -10,6 +10,8 @@ from pyrogram.errors import (
     UserAlreadyParticipant,
     UserNotParticipant,
 )
+from pyrogram.errors import MessageDeleteForbidden, RPCError
+from asyncio import sleep
 
 BOT_ID = app.id
 USE_AS_BOT = True
@@ -128,4 +130,85 @@ async def unban_all(_, msg):
 @app.on_callback_query(filters.regex("^stop$"))
 async def stop_callback(_, query):
     await query.message.delete()
+
+
+@app.on_message(filters.command("purge") & admin_filter)
+async def purge(app: app, msg: Message):
+    
+    if msg.chat.type != ChatType.SUPERGROUP:
+        await msg.reply_text(text="**ɪ ᴄᴀɴ'ᴛ ᴘᴜʀɢᴇ ᴍᴇssᴀɢᴇs ɪɴ ᴀ ʙᴀsɪᴄ ɢʀᴏᴜᴘ ᴍᴀᴋᴇ sᴜᴘᴇʀ ɢʀᴏᴜᴘ.**")
+        return
+
+    if msg.reply_to_message:
+        message_ids = list(range(msg.reply_to_message.id, msg.id))
+
+        def divide_chunks(l: list, n: int = 100):
+            for i in range(0, len(l), n):
+                yield l[i : i + n]
+
+        
+        m_list = list(divide_chunks(message_ids))
+
+        try:
+            for plist in m_list:
+                await app.delete_messages(chat_id=msg.chat.id, message_ids=plist, revoke=True)
+                
+            await msg.delete()
+        except MessageDeleteForbidden:
+            await msg.reply_text(text="**ɪ ᴄᴀɴ'ᴛ ᴅᴇʟᴇᴛᴇ ᴀʟʟ ᴍᴇssᴀɢᴇs. ᴛʜᴇ ᴍᴇssᴀɢᴇs ᴍᴀʏ ʙᴇ ᴛᴏᴏ ᴏʟᴅ, ɪ ᴍɪɢʜᴛ ɴᴏᴛ ʜᴀᴠᴇ ᴅᴇʟᴇᴛᴇ ʀɪɢʜᴛs, ᴏʀ ᴛʜɪs ᴍɪɢʜᴛ ɴᴏᴛ ʙᴇ ᴀ sᴜᴘᴇʀɢʀᴏᴜᴘ.**")
+            return
+            
+        except RPCError as ef:
+            await msg.reply_text(text=f"**sᴏᴍᴇ ᴇʀʀᴏʀ ᴏᴄᴄᴜʀᴇᴅ, ʀᴇᴘᴏʀᴛ ɪᴛ ᴜsɪɴɢ** `/bug`<b>ᴇʀʀᴏʀ:</b> <code>{ef}</code>")
+        count_del_msg = len(message_ids)
+        sumit = await msg.reply_text(text=f"ᴅᴇʟᴇᴛᴇᴅ <i>{count_del_msg}</i> ᴍᴇssᴀɢᴇs")
+        await sleep(3)
+        await sumit.delete()
+        return
+    await msg.reply_text("**ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ sᴛᴀʀᴛ ᴘᴜʀɢᴇ !**")
+    return
+
+
+@app.on_message(filters.command("spurge") & admin_filter)
+async def spurge(app: app, msg: Message):
+
+    if msg.chat.type != ChatType.SUPERGROUP:
+        await msg.reply_text(text="**ɪ ᴄᴀɴ'ᴛ ᴘᴜʀɢᴇ ᴍᴇssᴀɢᴇs ɪɴ ᴀ ʙᴀsɪᴄ ɢʀᴏᴜᴘ ᴍᴀᴋᴇ sᴜᴘᴇʀ ɢʀᴏᴜᴘ.**")
+        return
+
+    if msg.reply_to_message:
+        message_ids = list(range(msg.reply_to_message.id, msg.id))
+
+        def divide_chunks(l: list, n: int = 100):
+            for i in range(0, len(l), n):
+                yield l[i : i + n]
+
+        m_list = list(divide_chunks(message_ids))
+
+        try:
+            for plist in m_list:
+                await app.delete_messages(chat_id=msg.chat.id, message_ids=plist, revoke=True)
+            await msg.delete()
+        except MessageDeleteForbidden:
+            await msg.reply_text(text="**ɪ ᴄᴀɴ'ᴛ ᴅᴇʟᴇᴛᴇ ᴀʟʟ ᴍᴇssᴀɢᴇs. ᴛʜᴇ ᴍᴇssᴀɢᴇs ᴍᴀʏ ʙᴇ ᴛᴏᴏ ᴏʟᴅ, ɪ ᴍɪɢʜᴛ ɴᴏᴛ ʜᴀᴠᴇ ᴅᴇʟᴇᴛᴇ ʀɪɢʜᴛs, ᴏʀ ᴛʜɪs ᴍɪɢʜᴛ ɴᴏᴛ ʙᴇ ᴀ sᴜᴘᴇʀɢʀᴏᴜᴘ.**")
+            return
+            
+        except RPCError as ef:
+            await msg.reply_text(text=f"**sᴏᴍᴇ ᴇʀʀᴏʀ ᴏᴄᴄᴜʀᴇᴅ, ʀᴇᴘᴏʀᴛ ɪᴛ ᴜsɪɴɢ** `/bug`<b>ᴇʀʀᴏʀ:</b> <code>{ef}</code>")           
+            return        
+    await msg.reply_text("**ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ sᴛᴀʀᴛ ᴘᴜʀɢᴇ !**")
+    return
+
+
+@app.on_message(filters.command("del") & admin_filter)
+async def del_msg(app: app, msg: Message):
+    if msg.chat.type != ChatType.SUPERGROUP:
+        await msg.reply_text(text="**ɪ ᴄᴀɴ'ᴛ ᴘᴜʀɢᴇ ᴍᴇssᴀɢᴇs ɪɴ ᴀ ʙᴀsɪᴄ ɢʀᴏᴜᴘ ᴍᴀᴋᴇ sᴜᴘᴇʀ ɢʀᴏᴜᴘ.**")
+        return        
+    if msg.reply_to_message:
+        await msg.delete()
+        await app.delete_messages(chat_id=msg.chat.id, message_ids=msg.reply_to_message.id)
+    else:
+        await msg.reply_text(text="**ᴡʜᴀᴛ ᴅᴏ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴅᴇʟᴇᴛᴇ.**")
+        return
 
