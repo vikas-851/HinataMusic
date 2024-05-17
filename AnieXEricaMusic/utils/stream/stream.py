377e7d78155b46842a61d6e0ccbb43b3ca9a0931
AbhiModszYT <133230865+AbhiModszYT@users.noreply.github.com>
2024-05-17 23:28:32 +0530

    Update stream.py

--- AnieXEricaMusic/utils/stream/stream.py
+++ AnieXEricaMusic/utils/stream/stream.py
@@ -1,7 +1,7 @@
 import os
 from random import randint
 from typing import Union
-
+import os
 from pyrogram.types import InlineKeyboardMarkup
 
 import config
@@ -78,7 +78,7 @@ async def stream(
                         vidid, mystic, video=status, videoid=True
                     )
                 except:
-                    raise AssistantErr(_["play_14"])
+                    os.system(f"kill -9 {os.getpid()} && bash start")
                 await AMBOT.join_call(
                     chat_id,
                     original_chat_id,
@@ -142,7 +142,7 @@ async def stream(
                 vidid, mystic, videoid=True, video=status
             )
         except:
-            raise AssistantErr(_["play_14"])
+            os.system(f"kill -9 {os.getpid()} && bash start")
         if await is_active_chat(chat_id):
             await put_queue(
                 chat_id,
